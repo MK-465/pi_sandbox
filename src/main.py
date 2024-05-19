99294be5b86beaf488d260c6140425ba25de3ff8
Mykyta Kostiuk <MK.LIBMUT@gmail.com>
2024-05-19 18:41:27 +0300

    [PI-01] add basic files

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.service.basic_service import DummyService
+
+
+def main():
+    # Call a function from another module
+    service = DummyService("some name")
+    result = service.sum(1, 2)
+    print(f" Calculation result is: {result}")
+
+
+if __name__ == "__main__":
+    main()
